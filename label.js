603bb187fba74273d181fab1369aed79d3df098c
abisammy <minecraftlegolover@gmail.com>
2023-06-13 18:15:38 +0200

    Test formatting

--- label.js
+++ label.js
@@ -1,12 +1,18 @@
 const ExtensionUtils = imports.misc.extensionUtils;
 const CurrentExtension = ExtensionUtils.getCurrentExtension();
 
-let MAX_STRING_LENGTH,BUTTON_PLACEHOLDER,LABEL_FILTERED_LIST,
-	DIVIDER_STRING,REMOVE_TEXT_WHEN_PAUSED,
-	REMOVE_TEXT_PAUSED_DELAY,FIRST_FIELD,SECOND_FIELD,LAST_FIELD
-	MAX_STRING_LENGTH,DIVIDER_STRING;
+let MAX_STRING_LENGTH,
+	BUTTON_PLACEHOLDER,
+	LABEL_FILTERED_LIST,
+	DIVIDER_STRING,
+	REMOVE_TEXT_WHEN_PAUSED,
+	REMOVE_TEXT_PAUSED_DELAY,
+	FIRST_FIELD,
+	SECOND_FIELD,
+	LAST_FIELD;
+MAX_STRING_LENGTH, DIVIDER_STRING;
 
-function getSettings(){
+function getSettings() {
 	const settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.mpris-label');
 	MAX_STRING_LENGTH = settings.get_int('max-string-length');
 	REFRESH_RATE = settings.get_int('refresh-rate');
@@ -20,89 +26,81 @@ function getSettings(){
 	LAST_FIELD = settings.get_string('last-field');
 }
 
-var buildLabel = function buildLabel(players){
+var buildLabel = function buildLabel(players) {
 	getSettings();
 
 	// the placeholder string is a hint for the user to switch players
 	// it should appear if labelstring is empty and there's another player playing
 	// avoid returning empty strings directly, use "placeholder" instead
-	let placeholder = "";
-	if (players.activePlayers.length > 0 && players.selected.playbackStatus != "Playing")
+	let placeholder = '';
+	if (players.activePlayers.length > 0 && players.selected.playbackStatus != 'Playing')
 		placeholder = BUTTON_PLACEHOLDER;
 
-	if(REMOVE_TEXT_WHEN_PAUSED){
-		if(removeTextWhenPaused(players.selected))
-			return placeholder
+	if (REMOVE_TEXT_WHEN_PAUSED) {
+		if (removeTextWhenPaused(players.selected)) return placeholder;
 	}
 
 	let metadata = players.selected.metadata;
 
-	if(metadata == null)
-		return placeholder
+	if (metadata == null) return placeholder;
 
-	let fields = [FIRST_FIELD,SECOND_FIELD,LAST_FIELD]; //order is user-defined
-	fields.filter(field => field != ""); //discard fields that the user defined as empty(none)
+	let fields = [FIRST_FIELD, SECOND_FIELD, LAST_FIELD]; //order is user-defined
+	fields.filter((field) => field != ''); //discard fields that the user defined as empty(none)
 
-	let labelstring = "";
-	fields.forEach(field => {
-		let fieldString = players.selected.stringFromMetadata(field,metadata); //"extract" the string from metadata
+	let labelstring = '';
+	fields.forEach((field) => {
+		let fieldString = players.selected.stringFromMetadata(field, metadata); //"extract" the string from metadata
 		fieldString = parseMetadataField(fieldString); //check, filter, customize and add divider to the extracted string
 		labelstring += fieldString; //add it to the string to be displayed
 	});
 
-	labelstring = labelstring.substring(0,labelstring.length - DIVIDER_STRING.length); //remove the trailing divider
+	labelstring = labelstring.substring(0, labelstring.length - DIVIDER_STRING.length); //remove the trailing divider
 
-	if(labelstring.length === 0)
-		return placeholder
+	if (labelstring.length === 0) return placeholder;
 
-	return labelstring
-}
+	return labelstring;
+};
 
-function removeTextWhenPaused(player){
-	if (player.playbackStatus == "Playing")
-		return false
+function removeTextWhenPaused(player) {
+	if (player.playbackStatus == 'Playing') return false;
 
-	if ( (player.statusTimestamp / 1000) + REMOVE_TEXT_PAUSED_DELAY <= new Date().getTime() / 1000){
-		return true
+	if (player.statusTimestamp / 1000 + REMOVE_TEXT_PAUSED_DELAY <= new Date().getTime() / 1000) {
+		return true;
 	}
 }
 
 function parseMetadataField(data) {
-	if (data == undefined || data.length == 0)
-		return ""
+	if (data == undefined || data.length == 0) return '';
 
-	if (data.includes("xesam:") || data.includes("mpris:"))
-		return ""
+	if (data.includes('xesam:') || data.includes('mpris:')) return '';
 
 	//Replaces every instance of " | "
-	if(data.includes(" | "))
-		data = data.replace(/ \| /g, " / ");
+	if (data.includes(' | ')) data = data.replace(/ \| /g, ' / ');
 
-	if(LABEL_FILTERED_LIST){
+	if (LABEL_FILTERED_LIST) {
 		const CtrlCharactersRegex = new RegExp(/[.?*+^$[\]\\(){}|-]/, 'gi');
-		const sanitizedInput = LABEL_FILTERED_LIST.replace(CtrlCharactersRegex,"")
+		const sanitizedInput = LABEL_FILTERED_LIST.replace(CtrlCharactersRegex, '');
 
-		if(sanitizedInput != LABEL_FILTERED_LIST){
+		if (sanitizedInput != LABEL_FILTERED_LIST) {
 			const settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.mpris-label');
-			settings.set_string('label-filtered-list',sanitizedInput);
+			settings.set_string('label-filtered-list', sanitizedInput);
 		}
 
-		const filterRegex = new RegExp("(?:-|\\(|\\[).*(?:" + sanitizedInput.replace(",","|") + ").*(?:$|\\)|\\])","gi");
+		const filterRegex = new RegExp('(?:-|\\(|\\[).*(?:' + sanitizedInput.replace(',', '|') + ').*(?:$|\\)|\\])', 'gi');
 
-		data = data.replace(filterRegex,"");
+		data = data.replace(filterRegex, '');
 	}
 
 	//Cut string if it's longer than MAX_STRING_LENGTH, preferably in a space
-	if (data.length > MAX_STRING_LENGTH){
+	if (data.length > MAX_STRING_LENGTH) {
 		data = data.substring(0, MAX_STRING_LENGTH);
-		let lastIndex = data.lastIndexOf(" ");
-		if(lastIndex == -1)
-			lastIndex = data.length;
+		let lastIndex = data.lastIndexOf(' ');
+		if (lastIndex == -1) lastIndex = data.length;
 
-		data = data.substring(0, lastIndex) + "...";
+		data = data.substring(0, lastIndex) + '...';
 	}
 
 	data += DIVIDER_STRING;
 
-	return data
+	return data;
 }
--- players.js
+++ players.js
@@ -1,4 +1,4 @@
-const {Clutter,Gio,GLib,GObject,Shell,St} = imports.gi;
+const {Clutter, Gio, GLib, GObject, Shell, St} = imports.gi;
 const ExtensionUtils = imports.misc.extensionUtils;
 const CurrentExtension = ExtensionUtils.getCurrentExtension();
 
@@ -17,7 +17,7 @@ const mprisInterface = `
 		<property name="PlaybackStatus" type="s" access="read"/>
 	</interface>
 
-</node>`
+</node>`;
 
 const entryInterface = `
 <node>
@@ -25,7 +25,7 @@ const entryInterface = `
 		<property name="DesktopEntry" type="s" access="read"/>
 		<property name="Identity" type="s" access="read"/>
 	</interface>
-</node>`
+</node>`;
 
 const dBusInterface = `
 <node>
@@ -39,184 +39,187 @@ const dBusInterface = `
 			<arg direction="out" type="s"/>
 		</signal>
 	</interface>
-</node>`
+</node>`;
 
 var Players = class Players {
-	constructor(){
+	constructor() {
 		this.list = [];
-		this.activePlayers= [];
+		this.activePlayers = [];
 		const dBusProxyWrapper = Gio.DBusProxy.makeProxyWrapper(dBusInterface);
-		this.dBusProxy = dBusProxyWrapper(Gio.DBus.session,"org.freedesktop.DBus","/org/freedesktop/DBus",this._initList.bind(this));
+		this.dBusProxy = dBusProxyWrapper(
+			Gio.DBus.session,
+			'org.freedesktop.DBus',
+			'/org/freedesktop/DBus',
+			this._initList.bind(this),
+		);
 		this.settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.mpris-label');
 	}
-	pick(){
+	pick() {
 		const REMOVE_TEXT_WHEN_PAUSED = this.settings.get_boolean('remove-text-when-paused');
 		const AUTO_SWITCH_TO_MOST_RECENT = this.settings.get_boolean('auto-switch-to-most-recent');
 
-		if(this.list.length == 0){
+		if (this.list.length == 0) {
 			this.selected = null;
-			return this.selected
+			return this.selected;
 		}
 
-		if(!this.list.includes(this.selected))
-			this.selected = null
+		if (!this.list.includes(this.selected)) this.selected = null;
 
-		if(this.list.includes(this.selected) && !AUTO_SWITCH_TO_MOST_RECENT)
-			return this.selected
+		if (this.list.includes(this.selected) && !AUTO_SWITCH_TO_MOST_RECENT) return this.selected;
 
 		let newestTimestamp = 0;
 		let bestChoice = this.list[0];
 		let list = this.list;
 
-		if(AUTO_SWITCH_TO_MOST_RECENT){
-			if(this.activePlayers.length == 0)
-				return this.selected
+		if (AUTO_SWITCH_TO_MOST_RECENT) {
+			if (this.activePlayers.length == 0) return this.selected;
 
 			list = this.activePlayers;
 		}
 
-		list.forEach(player => {
-			if(player.statusTimestamp > newestTimestamp){
+		list.forEach((player) => {
+			if (player.statusTimestamp > newestTimestamp) {
 				newestTimestamp = player.statusTimestamp;
 				bestChoice = player;
 			}
 		});
 		this.selected = bestChoice;
-		return this.selected
+		return this.selected;
 	}
-	next(){
+	next() {
 		const AUTO_SWITCH_TO_MOST_RECENT = this.settings.get_boolean('auto-switch-to-most-recent');
 
 		let list = this.list;
 
-		if(AUTO_SWITCH_TO_MOST_RECENT)
-			list = this.activePlayers;
+		if (AUTO_SWITCH_TO_MOST_RECENT) list = this.activePlayers;
 
-		if(list.length < 2)
-			return this.selected
+		if (list.length < 2) return this.selected;
 
-		let newIndex = list.indexOf(this.selected)+1;
+		let newIndex = list.indexOf(this.selected) + 1;
 
-		if(this.selected == list[list.length-1])
-			newIndex = 0;
+		if (this.selected == list[list.length - 1]) newIndex = 0;
 
 		this.selected = list[newIndex];
 
-		if(AUTO_SWITCH_TO_MOST_RECENT){
+		if (AUTO_SWITCH_TO_MOST_RECENT) {
 			this.selected.statusTimestamp = new Date().getTime();
 		}
 
-		return this.selected
+		return this.selected;
 	}
-	_initList(){
+	_initList() {
 		let dBusList = this.dBusProxy.ListNamesSync()[0];
-		dBusList = dBusList.filter(element => element.startsWith("org.mpris.MediaPlayer2"));
+		dBusList = dBusList.filter((element) => element.startsWith('org.mpris.MediaPlayer2'));
 
 		this.unfilteredList = [];
-		dBusList.forEach(address => this.unfilteredList.push(new Player(address)));
+		dBusList.forEach((address) => this.unfilteredList.push(new Player(address)));
 
-		this.dBusProxy.connectSignal('NameOwnerChanged',this._updateList.bind(this));
+		this.dBusProxy.connectSignal('NameOwnerChanged', this._updateList.bind(this));
 	}
-	_updateList(proxy, sender, [name,oldOwner,newOwner]){
-		if(name.startsWith("org.mpris.MediaPlayer2")){
-			if(newOwner && !oldOwner){ //add player
+	_updateList(proxy, sender, [name, oldOwner, newOwner]) {
+		if (name.startsWith('org.mpris.MediaPlayer2')) {
+			if (newOwner && !oldOwner) {
+				//add player
 				let player = new Player(name);
 				this.unfilteredList.push(player);
-			}
-			else if (!newOwner && oldOwner){ //delete player
-				this.unfilteredList = this.unfilteredList.filter(player => player.address != name);
+			} else if (!newOwner && oldOwner) {
+				//delete player
+				this.unfilteredList = this.unfilteredList.filter((player) => player.address != name);
 			}
 		}
 	}
-	updateFilterList(){
-		if(!this.unfilteredList)
-			return
+	updateFilterList() {
+		if (!this.unfilteredList) return;
 
 		const SOURCES_BLACKLIST = this.settings.get_string('mpris-sources-blacklist');
 		const SOURCES_WHITELIST = this.settings.get_string('mpris-sources-whitelist');
 		let USE_WHITELIST = this.settings.get_boolean('use-whitelisted-sources-only');
 
-		if(!SOURCES_BLACKLIST || !SOURCES_WHITELIST)
-			this.list = this.unfilteredList;
+		if (!SOURCES_BLACKLIST || !SOURCES_WHITELIST) this.list = this.unfilteredList;
 
-		const blacklist = SOURCES_BLACKLIST.toLowerCase().replaceAll(' ','').split(',');
-		const whitelist = SOURCES_WHITELIST.toLowerCase().replaceAll(' ','').split(',');
+		const blacklist = SOURCES_BLACKLIST.toLowerCase().replaceAll(' ', '').split(',');
+		const whitelist = SOURCES_WHITELIST.toLowerCase().replaceAll(' ', '').split(',');
 
-		if(USE_WHITELIST && SOURCES_WHITELIST)
-			this.list = this.unfilteredList.filter(element => whitelist.includes(element.identity.toLowerCase().replaceAll(' ','')));
+		if (USE_WHITELIST && SOURCES_WHITELIST)
+			this.list = this.unfilteredList.filter((element) =>
+				whitelist.includes(element.identity.toLowerCase().replaceAll(' ', '')),
+			);
 
-		if(!USE_WHITELIST && SOURCES_BLACKLIST)
-			this.list = this.unfilteredList.filter(element => !blacklist.includes(element.identity.toLowerCase().replaceAll(' ','')));
+		if (!USE_WHITELIST && SOURCES_BLACKLIST)
+			this.list = this.unfilteredList.filter(
+				(element) => !blacklist.includes(element.identity.toLowerCase().replaceAll(' ', '')),
+			);
 	}
-	updateActiveList(){
+	updateActiveList() {
 		let actives = [];
-		this.list.forEach(player => {
-			if(player.playbackStatus == "Playing")
-				actives.push(player);
+		this.list.forEach((player) => {
+			if (player.playbackStatus == 'Playing') actives.push(player);
 		});
 		this.activePlayers = actives;
 	}
-}
+};
 
 class Player {
-	constructor(address){
+	constructor(address) {
 		this.address = address;
 		this.statusTimestamp = new Date().getTime();
 		this.albumArt = null;
 
 		const proxyWrapper = Gio.DBusProxy.makeProxyWrapper(mprisInterface);
-		this.proxy = proxyWrapper(Gio.DBus.session,this.address, "/org/mpris/MediaPlayer2",this.update.bind(this));
+		this.proxy = proxyWrapper(Gio.DBus.session, this.address, '/org/mpris/MediaPlayer2', this.update.bind(this));
 		this.proxy.connect('g-properties-changed', this.update.bind(this));
 
 		const entryWrapper = Gio.DBusProxy.makeProxyWrapper(entryInterface);
-		this.entryProxy = entryWrapper(Gio.DBus.session,this.address, "/org/mpris/MediaPlayer2",this._onEntryProxyReady.bind(this));
-
+		this.entryProxy = entryWrapper(
+			Gio.DBus.session,
+			this.address,
+			'/org/mpris/MediaPlayer2',
+			this._onEntryProxyReady.bind(this),
+		);
 	}
-	_onEntryProxyReady(){
+	_onEntryProxyReady() {
 		this.identity = this.entryProxy.Identity;
 		this.desktopEntry = this.entryProxy.DesktopEntry;
 
 		this.desktopApp = null;
 		let matchedEntries = [];
-		if(! (this.identity == null | undefined))
-			matchedEntries = Gio.DesktopAppInfo.search(this.identity);
+		if (!((this.identity == null) | undefined)) matchedEntries = Gio.DesktopAppInfo.search(this.identity);
 
-		if ( matchedEntries.length === 0 && !(this.desktopEntry == null | undefined) )//backup method using DesktopEntry info
+		if (matchedEntries.length === 0 && !((this.desktopEntry == null) | undefined))
+			//backup method using DesktopEntry info
 			matchedEntries = Gio.DesktopAppInfo.search(this.desktopEntry);
 
 		//de-nest matchedEntries. Gio.DesktopAppInfo.search returns a nested array
 		let entries = [];
-		matchedEntries.forEach(nest => {
-			nest.forEach(entry => {
+		matchedEntries.forEach((nest) => {
+			nest.forEach((entry) => {
 				entries.push(entry);
 			});
 		});
 		matchedEntries = entries;
 
-		if ( matchedEntries.length > 0 )
-			this.desktopApp = this._matchRunningApps(matchedEntries)
+		if (matchedEntries.length > 0) this.desktopApp = this._matchRunningApps(matchedEntries);
 
 		this.icon = this.getIcon(this.desktopApp);
 	}
-	_matchRunningApps(matchedEntries){
+	_matchRunningApps(matchedEntries) {
 		const activeApps = Shell.AppSystem.get_default().get_running();
 
-		const match = matchedEntries.find(entry => {
+		const match = matchedEntries.find((entry) => {
 			let playerObject = Shell.AppSystem.get_default().lookup_app(entry);
-			return activeApps.includes(playerObject)
+			return activeApps.includes(playerObject);
 		});
 
-		if(match)
-			return match
+		if (match) return match;
 
-		return matchedEntries[0]
+		return matchedEntries[0];
 	}
-	update(){
+	update() {
 		this.metadata = this.proxy.Metadata;
 
 		let playbackStatus = this.proxy.PlaybackStatus;
 
-		if(this.playbackStatus != playbackStatus){
+		if (this.playbackStatus != playbackStatus) {
 			this.playbackStatus = playbackStatus;
 			this.statusTimestamp = new Date().getTime();
 		}
@@ -225,93 +228,85 @@ class Player {
 		// metadata is a javascript object
 		// each "field" correspond to a string-keyed property on metadata
 		// each property contains a GLib.Variant object
-		if (Object.keys(this.metadata).includes(field)){
+		if (Object.keys(this.metadata).includes(field)) {
 			let variant = this.metadata[field];
 
-			if(variant.get_type().is_array())
-				return variant.get_strv()[0]
-			else
-				return variant.get_string()[0]
+			if (variant.get_type().is_array()) return variant.get_strv()[0];
+			else return variant.get_string()[0];
 		}
-		return ""
+		return '';
 	}
-	getArtUrlIcon(size){
-		const url = this.stringFromMetadata("mpris:artUrl",this.metadata);
-		if(url.length>0)
+	getArtUrlIcon(size) {
+		const url = this.stringFromMetadata('mpris:artUrl', this.metadata);
+		if (url.length > 0)
 			this.albumArt = new St.Icon({
 				gicon: Gio.Icon.new_for_string(url),
 				style_class: 'system-status-icon',
-				icon_size: size
-			})
+				icon_size: size,
+			});
 		else this.albumArt = null;
 
-		return this.albumArt
+		return this.albumArt;
 	}
-	getIcon(desktopApp){
+	getIcon(desktopApp) {
 		const settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.mpris-label');
 		const ICON_PLACE = settings.get_string('show-icon');
 		const Config = imports.misc.config;
 
 		let icon_left_padding = 0;
 		let icon_right_padding = 0;
-		if (Config.PACKAGE_VERSION.startsWith("3."))
-			if (ICON_PLACE == "right")
-				icon_left_padding = 3
-			else if (ICON_PLACE == "left")
-				icon_right_padding = 3
+		if (Config.PACKAGE_VERSION.startsWith('3.'))
+			if (ICON_PLACE == 'right') icon_left_padding = 3;
+			else if (ICON_PLACE == 'left') icon_right_padding = 3;
 
-			let icon = new St.Icon({
+		let icon = new St.Icon({
 			style_class: 'system-status-icon',
 			fallback_icon_name: 'audio-volume-high',
-			style: "padding-left: " + icon_left_padding + "px;padding-right: " + icon_right_padding + "px;"
+			style: 'padding-left: ' + icon_left_padding + 'px;padding-right: ' + icon_right_padding + 'px;',
 		});
 
-		if(desktopApp == null | undefined)
-			return icon
+		if ((desktopApp == null) | undefined) return icon;
 
 		let entry = Gio.DesktopAppInfo.new(desktopApp);
 		let gioIcon = entry.get_icon();
 		icon.set_gicon(gioIcon);
-		return icon
+		return icon;
 	}
 	toggleStatus() {
-		if (this.proxy.CanPlay && this.proxy.CanPause){
+		if (this.proxy.CanPlay && this.proxy.CanPause) {
 			this.proxy.PlayPauseRemote();
-			return
+			return;
 		}
-		if (this.proxy.PlaybackStatus == "Playing"){ // fallback to "Stop"
+		if (this.proxy.PlaybackStatus == 'Playing') {
+			// fallback to "Stop"
 			this.proxy.StopRemote();
-			return
+			return;
 		}
 	}
-	goNext(){
-		if (this.proxy.CanGoNext)
-			this.proxy.NextRemote()
+	goNext() {
+		if (this.proxy.CanGoNext) this.proxy.NextRemote();
 	}
-	goPrevious(){
-		if (this.proxy.CanGoPrevious)
-			this.proxy.PreviousRemote()
+	goPrevious() {
+		if (this.proxy.CanGoPrevious) this.proxy.PreviousRemote();
 	}
-	activatePlayer(){
+	activatePlayer() {
 		let focusedWindow = global.display.get_focus_window();
 		let playerWindow = this._matchAppWindow();
 		let currentWorkspace = global.workspace_manager.get_active_workspace();
 
-		if (!playerWindow)
-			return
+		if (!playerWindow) return;
 
-		if (focusedWindow == playerWindow){
-			if (currentWorkspace == this.previousWorkspace){ //go back to last workspace
-				if (this.playerWindowMinimized)
-					playerWindow.minimize();
+		if (focusedWindow == playerWindow) {
+			if (currentWorkspace == this.previousWorkspace) {
+				//go back to last workspace
+				if (this.playerWindowMinimized) playerWindow.minimize();
 
 				this.previousWindow.activate(global.get_current_time());
-			}
-			else if (this.previousWorkspace)//focus window on current workspace
+			} else if (this.previousWorkspace)
+				//focus window on current workspace
 				this.previousWorkspace.activate(global.get_current_time());
-		}
-		else{
-			if(this.desktopApp){
+		} else {
+			if (this.desktopApp) {
 				this.previousWorkspace = currentWorkspace;
 				this.previousWindow = focusedWindow;
 				this.playerWindowMinimized = playerWindow.minimized;
@@ -319,19 +314,19 @@ class Player {
 			}
 		}
 	}
-	_matchAppWindow(){//match player with window
+	_matchAppWindow() {
+		//match player with window
 		let app = Shell.AppSystem.get_default().lookup_app(this.desktopApp);
 		let appPID = app.get_pids();
 
 		let windows_list = global.get_window_actors();
-		windows_list = windows_list.filter(element => element.get_meta_window().get_window_type() == 0); //only keep normal windows
-		
-		for (const actor of windows_list) {//match window based on pid
+		windows_list = windows_list.filter((element) => element.get_meta_window().get_window_type() == 0); //only keep normal windows
+
+		for (const actor of windows_list) {
+			//match window based on pid
 			const window = actor.get_meta_window();
 			let windowPID = window.get_pid();
-			if (appPID.includes(windowPID))
-				return window;
+			if (appPID.includes(windowPID)) return window;
 		}
 	}
 }
-
